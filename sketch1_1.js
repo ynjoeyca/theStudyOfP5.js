1412b0419536a9eb26b2f2a76315d6586e381605
ynjoeyca <32921799+ynjoeyca@users.noreply.github.com>
2019-11-01 16:28:10 +0900

    Rename sketch1.js to sketch1_1.js

---
+++ sketch1_1.js
@@ -0,0 +1,13 @@
+let width= 500
+let height = 500
+function
+let radius = 400
+function setup(){
+  createCanvas(width, height);
+  
+  background(220);
+  
+  stroke(255, 0, 0)
+  strokeweight(6)
+ 
+}
